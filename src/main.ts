70ec84e7ef1864c647d9882d31a6c2fde98003de
Marquis Abah <marquis.abah@shuttlers.ng>
2024-05-31 15:20:19 +0100

    enabled cors

--- src/main.ts
+++ src/main.ts
@@ -1,32 +1,43 @@
-import { ValidationPipe } from "@nestjs/common"
-import { NestFactory } from "@nestjs/core"
-import { UserModule } from "./user/user.module"
-import { ProductModule } from "./product/product.module"
-import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger"
-import { AppModule } from "./app.module"
+import { ValidationPipe } from "@nestjs/common";
+import { NestFactory } from "@nestjs/core";
+import { UserModule } from "./user/user.module";
+import { ProductModule } from "./product/product.module";
+import { SwaggerModule, DocumentBuilder } from "@nestjs/swagger";
+import { CorsOptions } from "@nestjs/common/interfaces/external/cors-options.interface";
+import { AppModule } from "./app.module";
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule)
+  const app = await NestFactory.create(AppModule);
 
-	app.setGlobalPrefix("/api/v1").useGlobalPipes(
-		new ValidationPipe({
-			whitelist: true,
-		}),
-	)
-	const options = new DocumentBuilder()
-		.setTitle("API")
-		.setDescription("API description")
-		.setVersion("1.0")
-		.addTag("API")
-		.build()
-	const document = SwaggerModule.createDocument(app, options, {
-		include: [UserModule, ProductModule],
-	})
-	SwaggerModule.setup("api", app, document)
+  app.setGlobalPrefix("/api/v1").useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    })
+  );
+  const options = new DocumentBuilder()
+    .setTitle("API")
+    .setDescription("API description")
+    .setVersion("1.0")
+    .addTag("API")
+    .build();
+  const document = SwaggerModule.createDocument(app, options, {
+    include: [UserModule, ProductModule],
+  });
+  SwaggerModule.setup("api", app, document);
 
-	const PORT = process.env.PORT
+  // Define CORS options
+  const corsOptions: CorsOptions = {
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+    credentials: true,
+  }
+  
+  app.enableCors(corsOptions);
 
-	await app.listen(PORT)
+  const PORT = process.env.PORT;
+
+  await app.listen(PORT);
 }
 
-bootstrap()
+bootstrap();
